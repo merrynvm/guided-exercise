7bcbef88ae7c62eb2a10967a76fe80d422578c35
Merry <rozzimario18@gmail.com>
2024-12-11 21:03:34 +0100

    merged: guided-branch with master

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        String message = "Test esercizio guidato per Develhope. ";
+        String message2 = "Test esercizio v2.";
         System.out.println("Hello, World!");
+        System.out.println(message + message2);
     }
 }
